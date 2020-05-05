21c805e38e003dcb208479ad3adfe20f275c9783
Domonkos Suranyi <domonkos.suranyi@omixon.com>
2020-05-05 17:12:26 +0200

    Add unit tests to rotate_towards_mouse

--- src/systems/player_movement.rs
+++ src/systems/player_movement.rs
@@ -52,7 +52,9 @@ impl<'s> System<'s> for PlayerMovementSystem {
 
     fn run(&mut self, (mut transforms, mut velocities, players, input): Self::SystemData) {
         for (_player, mut velocity, mut transform) in (&players, &mut velocities, &mut transforms).join() {
-            rotate_toward_mouse(&mut transform, &input);
+            if let Some(mouse_pos) = input.mouse_position() {
+                rotate_toward_mouse(&mut transform, mouse_pos);
+            }
             update_velocity(&transform, &input, &mut velocity);
         }
     }
@@ -61,24 +63,21 @@ impl<'s> System<'s> for PlayerMovementSystem {
 
 fn rotate_toward_mouse(
     transform: &mut Transform,
-    input: &InputHandler<StringBindings>,
+    (mouse_x, mouse_y): (f32, f32),
 ) {
-    if let Some((x, y)) = input.mouse_position() {
-        // Calculate the vector from player position to mouse cursor
-        let mouse_direction = Vector2::new(
-            x - transform.translation().x,
-            transform.translation().y - y
-        );
+    // Calculate the vector from player position to mouse cursor
+    let mouse_direction = Vector2::new(
+        mouse_x - transform.translation().x,
+        transform.translation().y - mouse_y
+    );
 
-        let base_vector = Vector2::new(0.0, -1.0);
-        let mut angle = base_vector.angle(&mouse_direction);
+    let base_vector = Vector2::new(0.0, -1.0);
+    let mut angle = base_vector.angle(&mouse_direction);
 
-        if mouse_direction.x < 0.0 {
-            angle = 2.0 * std::f32::consts::PI - angle;
-        }
-
-        transform.set_rotation_2d(angle);
+    if mouse_direction.x < 0.0 {
+        angle = 2.0 * std::f32::consts::PI - angle;
     }
+    transform.set_rotation_2d(angle);
 }
 
 const PLAYER_MAX_WALK_SPEED: f32 = 64.0;
@@ -132,5 +131,53 @@ fn as_vector2(move_dir: MoveDirection) -> Vector2<f32> {
         MoveDirection::Backward => Vector2::new(0.0, PLAYER_MAX_WALK_SPEED / 2.0),
         MoveDirection::StrafeLeft => Vector2::new(PLAYER_MAX_WALK_SPEED / 2.0, 0.0),
         MoveDirection::StrafeRight => Vector2::new(-PLAYER_MAX_WALK_SPEED / 2.0, 0.0)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::f32::consts::PI;
+    use amethyst::core::Transform;
+    use amethyst::core::math::Vector3;
+
+    fn assert_f32_equals(exp: f32, act:f32, threshold: f32) {
+        let diff = (exp - act).abs();
+
+
+        assert!(diff < threshold, "Expected: {}, Actual: {}", exp, act);
+    }
+
+    mod test_rotate_toward_mouse {
+        use super::*;
+
+        macro_rules! test_rotate_toward_mouse {
+            ($($name:ident: $player_coord:expr, $cursor_coord:expr, $expected:expr,)*) => {
+                $(
+                    #[test]
+                    fn $name() {
+                        let player = $player_coord;
+                        let transform = &mut Transform::default();
+                        transform.set_translation_x(player.0);
+                        transform.set_translation_y(player.1);
+
+                        rotate_toward_mouse(transform, $cursor_coord);
+
+                        let angle = transform.rotation().axis().map(|vec| vec.z).unwrap_or(1.0) * transform.rotation().angle();
+
+                        // sin is being called to normalize the angles (e.g. -PI = PI)
+                        assert_f32_equals(f32::sin($expected), angle.sin(), 0.000001);
+                    }
+                )*
+            }
+        }
+
+        test_rotate_toward_mouse! {
+            cursor_up: (3.0, 3.0), (3.0, 2.0), PI,
+            cursor_left: (3.0, 3.0), (2.0, 3.0), -PI/2.0,
+            cursor_down: (3.0, 3.0), (3.0, 4.0), 0.0,
+            cursor_right: (3.0, 3.0), (4.0, 3.0), PI/2.0,
+            cursor_upright_45deg: (3.0, 3.0), (4.0, 2.0), 3.0*PI/4.0,
+        }
     }
 }
