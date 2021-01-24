00973a5bdb9976287053e6944095b670618fc252
Marton Suranyi <marton.suranyi@gmail.com>
2021-01-24 12:50:33 +0100

    Add Weapon component with a lot of details, some of them is not implemented yet.
    Implemented:
    - fire rate control
    - multi shot type handling (single shot vs. auto shot)
    - bullet speed
    - weapon range

--- src/entities/bullet.rs
+++ src/entities/bullet.rs
@@ -1,14 +1,19 @@
 
 use amethyst::{
     core::transform::Transform,
+    core::math::Vector2,
     ecs::prelude::{Builder, Entities, LazyUpdate, ReadExpect},
     renderer::SpriteRender
 };
 
-use crate::components::{Velocity, DistanceLimit};
+use crate::components::{Velocity, DistanceLimit, WeaponDetails};
 
-pub fn spawn_bullet(transform: Transform, velocity: Velocity, sprite_render: SpriteRender, distance_limit: DistanceLimit,
-                    entities: &Entities, lazy_update: &ReadExpect<LazyUpdate>) {
+pub fn spawn_bullet(transform: Transform, direction: Vector2<f32>, weapon_details: &WeaponDetails, sprite_render: SpriteRender,
+                    entities: &Entities, lazy_update: &ReadExpect<LazyUpdate>)
+{
+    let distance_limit = DistanceLimit::new(weapon_details.distance);
+    let velocity = Velocity(direction * weapon_details.bullet_speed);
+
     lazy_update
         .create_entity(entities)
         .with(transform)
--- src/systems/player_shooter.rs
+++ src/systems/player_shooter.rs
@@ -1,13 +1,13 @@
 
 use amethyst::input::InputHandler;
 use amethyst::derive::SystemDesc;
-use amethyst::ecs::{Read, System, SystemData, ReadStorage, ReadExpect, Entities};
-use amethyst::core::{Transform, math::{Vector3, Vector2}};
+use amethyst::ecs::{Read, System, SystemData, ReadStorage, ReadExpect, Entities, WriteStorage};
+use amethyst::core::{Transform, Time, math::{Vector3, Vector2}};
 use amethyst::ecs::prelude::LazyUpdate;
 use amethyst::ecs::prelude::Join;
 
 use crate::systems::player_movement::{ActionBinding, MovementBindingTypes};
-use crate::components::{Player, Velocity, DistanceLimit};
+use crate::components::{Player, Velocity, Weapon};
 use crate::entities::spawn_bullet;
 use crate::resources::{SpriteResource, SpriteId};
 
@@ -19,24 +19,34 @@ impl<'s> System<'s> for PlayerShooterSystem {
         Entities<'s>,
         ReadStorage<'s, Transform>,
         ReadStorage<'s, Player>,
+        WriteStorage<'s, Weapon>,
         Read<'s, InputHandler<MovementBindingTypes>>,
+        Read<'s, Time>,
         ReadExpect<'s, SpriteResource>,
         ReadExpect<'s, LazyUpdate>
     );
 
-    fn run(&mut self, (entities, transforms, players, input, sprites, lazy_update): Self::SystemData) {
-        for (_player, player_transform) in (&players, &transforms).join() {
+    fn run(&mut self, (entities, transforms, players, mut weapons, input, time, sprites, lazy_update): Self::SystemData) {
+        for (_player, player_transform, mut weapon) in (&players, &transforms, &mut weapons).join() {
             if input.action_is_down(&ActionBinding::Shoot).unwrap_or(false)
             {
-                let mut bullet_transform = Transform::default();
-                bullet_transform.set_translation(*player_transform.translation());
-                bullet_transform.set_rotation(*player_transform.rotation());
+                if weapon.is_allowed_to_shoot(time.absolute_time_seconds())
+                {
+                    let mut bullet_transform = Transform::default();
+                    bullet_transform.set_translation(*player_transform.translation());
+                    bullet_transform.set_rotation(*player_transform.rotation());
 
-                let direction3d = (bullet_transform.rotation() * Vector3::y()).normalize();
-                let speed = 100.0;
-                let velocity = Velocity(Vector2::new(-direction3d.x * speed, -direction3d.y * speed));
-                let distance_limit = DistanceLimit::new(100.0);
-                spawn_bullet(bullet_transform, velocity, sprites.sprite_render_for(SpriteId::Bullet), distance_limit, &entities, &lazy_update);
+                    let direction3d = (bullet_transform.rotation() * Vector3::y()).normalize();
+                    let direction2d = Vector2::new(-direction3d.x, -direction3d.y);
+
+                    spawn_bullet(bullet_transform, direction2d, &weapon.details, sprites.sprite_render_for(SpriteId::Bullet), &entities, &lazy_update);
+                    weapon.last_shot_time = time.absolute_time_seconds();
+                    weapon.input_lifted = false;
+                }
+            }
+            else
+            {
+                weapon.input_lifted = true;
             }
         }
     }
