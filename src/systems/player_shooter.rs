817751c3bd2a7947bae0dc6fef67b12123412795
abelsuranyi <abel.suranyi@balabit.com>
2021-02-07 22:52:34 +0100

    player shooter system uses Input component

--- src/systems/player_shooter.rs
+++ src/systems/player_shooter.rs
@@ -1,5 +1,4 @@
 
-use amethyst::input::InputHandler;
 use amethyst::derive::SystemDesc;
 use amethyst::ecs::{Read, System, SystemData, ReadStorage, ReadExpect, Entities, WriteStorage};
 use amethyst::core::{Transform, Time, math::{Vector3, Vector2}};
@@ -8,10 +7,10 @@ use amethyst::ecs::prelude::Join;
 use amethyst::assets::AssetStorage;
 use amethyst::audio::{Source, output::Output};
 
-use crate::systems::player_movement::{ActionBinding, MovementBindingTypes};
 use crate::components::{Player, Weapon, BoundingCircle};
 use crate::entities::spawn_bullet;
 use crate::resources::{SpriteResource, SpriteId, Sounds};
+use westiny_common::components::Input;
 
 #[derive(SystemDesc)]
 pub struct PlayerShooterSystem;
@@ -21,9 +20,9 @@ impl<'s> System<'s> for PlayerShooterSystem {
         Entities<'s>,
         ReadStorage<'s, Transform>,
         ReadStorage<'s, Player>,
+        ReadStorage<'s, Input>,
         ReadStorage<'s, BoundingCircle>,
         WriteStorage<'s, Weapon>,
-        Read<'s, InputHandler<MovementBindingTypes>>,
         Read<'s, Time>,
         ReadExpect<'s, SpriteResource>,
         ReadExpect<'s, LazyUpdate>,
@@ -32,9 +31,9 @@ impl<'s> System<'s> for PlayerShooterSystem {
         Read<'s, Output>
     );
 
-    fn run(&mut self, (entities, transforms, players, bounds, mut weapons, input, time, sprites, lazy_update, audio_storage, sounds, sound_output): Self::SystemData) {
-        for (_player, player_transform, player_bound, mut weapon) in (&players, &transforms, &bounds, &mut weapons).join() {
-            if input.action_is_down(&ActionBinding::Shoot).unwrap_or(false)
+    fn run(&mut self, (entities, transforms, players, inputs, bounds, mut weapons, time, sprites, lazy_update, audio_storage, sounds, sound_output): Self::SystemData) {
+        for (_player, input, player_transform, player_bound, mut weapon) in (&players, &inputs, &transforms, &bounds, &mut weapons).join() {
+            if input.shoot
             {
                 if weapon.is_allowed_to_shoot(time.absolute_time_seconds())
                 {
