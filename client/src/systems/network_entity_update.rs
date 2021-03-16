493f858d5a23ab2948db19267798ce3728e4b25d
Domonkos Suranyi <suranyi.domi@gmail.com>
2021-03-16 14:28:53 +0100

    Spawn a corpse where a player died

--- client/src/systems/network_entity_update.rs
+++ client/src/systems/network_entity_update.rs
@@ -6,16 +6,18 @@ use amethyst::{
     prelude::Builder
 };
 use derive_new::new;
-use westiny_common::network::EntityState;
 use amethyst::core::ecs::{ReadStorage, WriteStorage, Join, Entities, LazyUpdate, world::LazyBuilder};
-use westiny_common::components::{NetworkId, EntityType};
+use westiny_common::network::{EntityState, PlayerDeath};
+use westiny_common::components::{NetworkId, EntityType, Lifespan};
 use westiny_common::resources::SpriteId;
-use amethyst::core::Transform;
+use amethyst::core::{Transform, Time};
 use std::collections::HashMap;
+use std::time::Duration;
 use amethyst::shred::ReadExpect;
 
 use crate::resources;
 use crate::entities::{create_player, create_character};
+use crate::resources::SpriteResource;
 
 const CORPSE_HEIGHT: f32 = 0.1;
 
@@ -23,31 +25,41 @@ const CORPSE_HEIGHT: f32 = 0.1;
 #[system_desc(name(NetworkEntityStateUpdateSystemDesc))]
 pub struct NetworkEntityStateUpdateSystem {
     #[system_desc(event_channel_reader)]
-    reader: ReaderId<Vec<EntityState>>,
+    entity_state_reader: ReaderId<Vec<EntityState>>,
+
+    #[system_desc(event_channel_reader)]
+    death_reader: ReaderId<PlayerDeath>,
 }
 
 impl<'s> System<'s> for NetworkEntityStateUpdateSystem {
     type SystemData = (
         Read<'s, EventChannel<Vec<EntityState>>>,
+        Read<'s, EventChannel<PlayerDeath>>,
         ReadStorage<'s, NetworkId>,
         WriteStorage<'s, Transform>,
         Entities<'s>,
         ReadExpect<'s, resources::SpriteResource>,
         ReadExpect<'s, resources::PlayerNetworkId>,
-        Read<'s, LazyUpdate>,
+        ReadExpect<'s, LazyUpdate>,
+        ReadExpect<'s, Time>,
     );
 
     fn run(&mut self,
            (
-               events,
+               entity_state_event_channel,
+               death_event_channel,
                network_ids,
                mut transforms,
                entities,
                sprite_resource,
                player_net_id,
                lazy,
+               time,
            ): Self::SystemData) {
-        let mut entity_states: HashMap<_, _> = events.read(&mut self.reader).flat_map(|vec| vec.iter()).map(|entity_state| (entity_state.network_id, entity_state)).collect();
+        let mut entity_states: HashMap<_, _> = entity_state_event_channel.read(&mut self.entity_state_reader)
+            .flat_map(|vec| vec.iter())
+            .map(|entity_state| (entity_state.network_id, entity_state))
+            .collect();
 
         for (net_id, transform) in (&network_ids, &mut transforms).join() {
             if let Some(state) = entity_states.get(net_id) {
@@ -73,6 +85,35 @@ impl<'s> System<'s> for NetworkEntityStateUpdateSystem {
                 EntityType::Player => create_character(lazy.create_entity(&entities), ||{ lazy.create_entity(&entities)}, &sprite_resource, net_id, transform)
             };
         }
+
+        let deaths = death_event_channel.read(&mut self.death_reader);
+        if let Err(error) = Self::handle_deaths(deaths, &lazy, &sprite_resource, &entities, time.absolute_time()) {
+            log::error!("Failed to handle player death event: {}", error);
+        }
+    }
+}
+
+impl<'s> NetworkEntityStateUpdateSystem {
+    fn handle_deaths<D: IntoIterator<Item=&'s PlayerDeath>>(
+        deaths: D,
+        lazy: &LazyUpdate,
+        sprite_resource: &SpriteResource,
+        entities: &Entities<'_>,
+        current_time: Duration,
+    ) -> anyhow::Result<()> {
+        deaths.into_iter().for_each(|death| {
+            let transform = {
+                let mut transform = Transform::default();
+                transform.set_translation_xyz(death.position.x, death.position.y, -0.9);
+                transform
+            };
+            lazy.create_entity(entities)
+                .with(transform)
+                .with(sprite_resource.sprite_render_for(SpriteId::Corpse))
+                .with(Lifespan::new(60.0, current_time))
+                .build();
+        });
+        Ok(())
     }
 }
 
